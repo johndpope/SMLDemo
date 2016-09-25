f2a7a8674d213601bc7ff5a8ef1c945d2930c6af
yuji <hatsuneyuji@yahoo.co.jp>
2016-09-25 07:11:05 -0700

    fixes

--- Package.swift
+++ Package.swift
@@ -2,5 +2,5 @@ import PackageDescription
 
 let package = Package(
     name: "SMLDemo",
-    dependencies: [.Package(url "https://github.com/michael-yuji/SML.git", versions: Version(0,0,0)..<Version(1,0,0))]
+    dependencies: [.Package(url: "https://github.com/michael-yuji/SML.git", versions: Version(0,0,0)..<Version(1,0,0))]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,30 @@
-print("Hello, world!")
+
+import SMLKit
+import SXF97
+import Foundation
+
+let blogIndex = ProcessInfo.processInfo.arguments[1]
+let root = ProcessInfo.processInfo.arguments[2]
+
+if ProcessInfo.processInfo.arguments.count != 3 {
+    /* so called index just a file indexing my blogs post to a url reference */
+    /* /root/sites is the directory I store the formmated blog content */
+    print("Usage: ./SMLDemo [path/to/index] [path/to/root/sites/]")
+    exit(0)
+}
+
+SMLInit(moduleName: "blogs") {
+    request -> HTTPResponse? in
+    if request.uri.path == "/blogs.html" {
+        return HTTPResponse(status: 200, text: Blogs())
+    }
+    
+    if request.uri.pathComponents.count >= 2 {
+        if request.uri.pathComponents[1] == "sites" {
+            let d = BlogsResources.generate_html(for: request.uri.path, root: root, base: "sites")
+            return HTTPResponse(status: .success(.ok), text: d)
+        }
+    }
+    
+    return nil
+}
